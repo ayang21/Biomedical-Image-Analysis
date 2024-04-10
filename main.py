20afeecfe28c80aed0115a0bc54c651e01966e47
patrickwu2001 <wupf@bc.edu>
2024-04-10 17:32:57 -0400

    load dataset

--- main.py
+++ main.py
@@ -46,3 +46,59 @@ for i in range(len(ann_directory)):
                     break
             else:
                 shutil.copy(img_directory[i] + imgname, tb_destination_path + imgname)
+
+#Load and preprocess data
+#Splitting the dataset into train and test using the SubsetSampler class
+#To apply two different sets of transforms to train and test, I'm loading the dataset twice.
+#I've applied resize so as to make the images smaller and thus model training faster. Models trained on smaller images tend to not overfit and generalize well for larger images.
+#I've also added a random crop and random flip to augment the dataset further.
+
+#The mean and standard deviation of the ImageNet data Alexnet was trained on.
+mean = [0.485, 0.456, 0.406]
+std  = [0.229, 0.224, 0.225] 
+test_size = 0.30
+random_seed = 24
+num_workers = 0
+batch_size = 8
+train_transform = transforms.Compose([
+    transforms.Resize(256),
+    transforms.RandomCrop(224),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=mean,
+                         std=std)
+])
+test_transform = transforms.Compose([
+    transforms.Resize(256),    
+    transforms.RandomCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=mean,
+                         std=std)
+])
+train_dataset = datasets.ImageFolder(root=dataset_dir,
+                                  transform=train_transform)
+
+test_dataset = datasets.ImageFolder(root=dataset_dir,
+                                  transform=test_transform)
+dataset_size = len(train_dataset)
+indices = list(range(dataset_size))
+split = int(np.floor(test_size * dataset_size))
+np.random.seed(random_seed)
+np.random.shuffle(indices)
+train_idx, test_idx = indices[split:], indices[:split]
+train_sampler = SubsetRandomSampler(train_idx)
+test_sampler = SubsetRandomSampler(test_idx)
+
+train_loader = torch.utils.data.DataLoader(train_dataset, 
+                                           batch_size=batch_size, 
+                                           sampler=train_sampler,
+                                           num_workers=num_workers)
+
+test_loader = torch.utils.data.DataLoader(test_dataset, 
+                                          batch_size=batch_size, 
+                                          sampler=test_sampler,
+                                          num_workers=num_workers)
+dataloaders = {
+    'train': train_loader,
+    'test': test_loader
+}
